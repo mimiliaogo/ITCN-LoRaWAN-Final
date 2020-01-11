de5ab281ea29eda1bd489905d2548181cf3d9036
BWbwchen <tim.chenbw@gmail.com>
2020-01-11 17:11:04 +0800

    Finish notification
    1. Modified calculate_date_filter to enable time interval
    2. Add time interval request data and can detect vibrate
    3. Message same as telegram bot

--- scripts/BikeLocation.js
+++ scripts/BikeLocation.js
@@ -3,7 +3,7 @@ var panorama;
 var now_location = { lat: 0, lng: 0 };
 var AccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjY1NmY0NjVhZDdlZTRmMmU2MGY4YTFlODRhZmFmNTUzYzcwYTBhYTk4ZjE3ZjFiMTM1ZWJjMGMwNzcxZWJiMzQyMjAyYzJmZjlkNzBiYWVhIn0.eyJhdWQiOiIyIiwianRpIjoiNjU2ZjQ2NWFkN2VlNGYyZTYwZjhhMWU4NGFmYWY1NTNjNzBhMGFhOThmMTdmMWIxMzVlYmMwYzA3NzFlYmIzNDIyMDJjMmZmOWQ3MGJhZWEiLCJpYXQiOjE1NzYyMjAzNzQsIm5iZiI6MTU3NjIyMDM3NCwiZXhwIjoxNjA3ODQyNzc0LCJzdWIiOiIyNDMiLCJzY29wZXMiOltdfQ.EYcCD-gsNSKSK4YgTS1BhZytAlHFrQMe8v7IlzABe8Iufj1Hglt9sVe327c0s2_YqJS3WSGv3CUu2ogTtA1KzzRuGXk0a0nT3NXsNNhxrDI6KJaSPu6dEsYrUEnU2d3UbX9hHz_LwLuki_DR_L4j_olfyJ8unM-eJp4hSQNrK85Z5owWQPSlRvIWYoPyekqH5bU59KTUU4gSYMYKryQ5PMCm1hhmRe_UneGzUDD3ofiaAxV1yHMp8esjGh6pTNB5FLSqEq036H81UxTYzvXSEL0p7sQPbD7jqw7RHRwwXauVcjV42Zp-TdjYzTsACGzhj7FjoDnBxmhWWXont1IPpp1ChrIrD_W4lHub2vp1zD6lg5MROWEIDVQ3ejzMZYFu7xhqQux8Idjj30KrGt7Acvcv-hth36EG4dHJM4uLCrFW6PM2mxPWHQMvr2yd7P9MtV8ZW9DdBoJVGwoJka4XNLAbZGw55m7HXKZPEcvvlDPpmvTOHKk2DDR7uzRfXPvc8xDNfDZx4ksqMpvepuzJkBwgkVn1CZi3rsDDFgwZVWQkMEtRKlKZj4TaodiIf-iKfg-p6uP-0mn6orj0CI4LafyuFKJhLT8Si8NTNsUPYYYhfk8_87tSGWRmZb66OjcisU0NqD7TAiZzWv6RpRGHIOE3UQ3RPwvrBx7OH2F7L6M";
 var macaddr = "?macaddr=" + "aa37d395"; // the mac address of the LoRaWan device
-var sendNotificationTimeInterval = 1000;
+var sendNotificationTimeInterval = 10000;
 
 $(document).ready(function () {
     $("#btn_get_bike_loc").click(function () {
@@ -100,13 +100,24 @@ function calculate_date_filter() {
     else date_s += (date.getMonth() + 1).toString() + "-";
     if (date.getDate() < 10) date_s += "0" + (date.getDate()).toString() + " ";
     else date_s += (date.getDate()).toString() + " ";
-    var hour_before, hour_after;
-    if ((date.getHours() - 1) < 10) hour_before = "0" + (date.getMonth() - 1).toString();
-    else hour_before = ((date.getHours() - 1)).toString();
-    if ((date.getHours() + 1) < 10) hour_after = "0" + (date.getMonth() + 1).toString();
-    else hour_after = ((date.getHours() + 1)).toString();
+
+    var secondWithInterval = new Date();
+    var timeStringInterval ;
+    secondWithInterval.setTime(date.getTime() - sendNotificationTimeInterval);
+    timeStringInterval = ('0' + secondWithInterval.getHours()).slice(-2) + ':'
+                    + ('0' + (secondWithInterval.getMinutes())).slice(-2) + ':'
+                    + ('0' + (secondWithInterval.getSeconds())).slice(-2) ;
+
+    var secondNow = new Date();
+    var timeStringNow ;
+    timeStringNow = ('0' + secondNow.getHours()).slice(-2) + ':'
+                    + ('0' + (secondNow.getMinutes())).slice(-2) + ':'
+                    + ('0' + (secondNow.getSeconds())).slice(-2) ;
+
+    
     //var date_filter = "&date_filter=" + date_s + hour_before + ":00:00+-+" + date_s + hour_after + ":00:00";
-    var date_filter = "&date_filter=" + "2020-01-01 11:22:00+-+2020-01-10 11:22:00";
+    var date_filter = "&date_filter=" + date_s + timeStringInterval+ "+-+" + date_s + timeStringNow;
+    //var date_filter = "&date_filter=" + "2020-01-10 23:58:00+-+2020-01-10 23:58:10";
     return date_filter;
 }
 
@@ -126,61 +137,18 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function notify() {
-    var closeNotificationTime = 1000;
-    // check if the browser supports notifications
-    if (!("Notification" in window)) {
-        alert("This browser does not support desktop notification");
-    }
-    // check if the user is okay to get some notification
-    else if (Notification.permission === "granted") {
-        // If it's okay let's create a notification
-        // fsm()
-        var notification = new Notification("Vibrate!!");
-    }
-    //  ask the user for permission
-    else if (Notification.permission !== 'denied') {
-        Notification.requestPermission(function (permission) {
-
-            // Whatever the user answers, we make sure we store the information
-            if (!('permission' in Notification)) {
-                Notification.permission = permission;
-            }
-
-            // If the user is okay, let's create a notification
-            if (permission === "granted") {
-                // fsm()
-                var notification = new Notification("Hi there!");
-            }
-        });
-    } else {
-        alert(`Permission is ${Notification.permission}`);
-    }
-    setTimeout(function() { notification.close() }, closeNotificationTime);
-}
-var previousState = null;
-fsm();
-function fsm () {
-    var nowTimeData = null;
-    // request data 
+    // request data
     $.ajax({
         type: "POST",
         url: "https://campus.kits.tw/ICN_API" + macaddr + calculate_date_filter(),
         dataType: "json",
         async: false,
         success: function (response) {
-            // TODO : assume this get the now time data
-            let flag = 1;
-            for (let index = 0; index < response.length; index++) {
-                if (response[index]['acc_x'] != null ||
-                    response[index]['acc_y'] != null ||
-                    response[index]['acc_z'] != null)  {
-                    console.log(response[index]);
-                    nowTimeData = response[index];
-                    flag = 0;
-                }
-            }
-            if (flag == 1)  {
-                console.log("fuck");
+            console.log(response);
+            if (response[0]['acc_x'] != null || 
+                response[0]['acc_y'] != null ||
+                response[0]['acc_z'] != null ) {
+                pushNotification();
             }
         },
         headers: {
@@ -195,11 +163,54 @@ function fsm () {
                 alert("API is sleeping !");
         },
     })
-    if (previousState == null)  {
-        if (nowTimeData != null ) {
-            // notificate
-            var notification = new Notification("Vibrate!!");
-        }
+}
+
+function pushNotification () {
+    // now time 
+    var date = new Date(); // get current time
+    var date_s = date.getFullYear().toString() + "-"; // date in string
+    if (date.getMonth() + 1 < 10) date_s += "0" + (date.getMonth() + 1).toString() + "-";
+    else date_s += (date.getMonth() + 1).toString() + "-";
+    if (date.getDate() < 10) date_s += "0" + (date.getDate()).toString() + " ";
+    else date_s += (date.getDate()).toString() + " ";
+
+    var secondNow = new Date();
+    var timeStringNow ;
+    timeStringNow = ('0' + secondNow.getHours()).slice(-2) + ':'
+                    + ('0' + (secondNow.getMinutes())).slice(-2) + ':'
+                    + ('0' + (secondNow.getSeconds())).slice(-2) ; 
+    var nowTimeString = date_s + timeStringNow;
+
+    var notifactionOption = {
+        body : "Detect bike moving at " + nowTimeString
     }
-    previousState = nowTimeData;
+
+    var closeNotificationTime = 5000;
+    // check if the browser supports notifications
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+    // check if the user is okay to get some notification
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification("Vibrate!!", notifactionOption);
+    }
+    //  ask the user for permission
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+
+            // Whatever the user answers, we make sure we store the information
+            if (!('permission' in Notification)) {
+                Notification.permission = permission;
+            }
+
+            // If the user is okay, let's create a notification
+            if (permission === "granted") {
+                var notification = new Notification("Vibrate!!", notifactionOption);
+            }
+        });
+    } else {
+        alert(`Permission is ${Notification.permission}`);
+    }
+    setTimeout(function() { notification.close() }, closeNotificationTime);
 }
